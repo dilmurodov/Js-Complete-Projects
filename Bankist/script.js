c3f80fa6222b1dde08c98f038dd17dbed38c9c96
dilmurodov <tolibdilmurodov98@gmail.com>
2022-03-24 03:33:10 +0500

    [docs] complate project ✨✔✔

--- Bankist/script.js
+++ Bankist/script.js
@@ -346,19 +346,6 @@ const setTimer = function () {
 
 }
 
-
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
-
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
 /////////////////////////////////////////////////
 
 // let currentTime = new Date();
